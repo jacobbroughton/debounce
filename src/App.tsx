31cbf5f3d7442781962b1ef8f7e6ddae5c69f8eb
Jacob <jlbroughton88@gmail.com>
2025-04-05 12:02:51 -0400

    basic debounce functionality added, initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [status, setStatus] = useState<
+    "Waiting for input" | "Typing" | "Async function running"
+  >("Waiting for input");
+  const [inputValue, setInputValue] = useState<string>("");
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    if (status !== "Typing") setStatus("Typing");
+    setInputValue(e.target.value);
+  }
+
+  async function asyncFunction() {
+    setStatus("Async function running");
+
+    setTimeout(() => {
+      setStatus("Waiting for input");
+    }, 1000);
+  }
+
+  useEffect(() => {
+    const interval = setTimeout(asyncFunction, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [inputValue]);
+
+  return (
+    <>
+      <input onChange={handleChange} />
+      <p>{status}</p>
+    </>
+  );
+}
+
+export default App;
